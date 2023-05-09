49aa758d205ca085d52b96bf1ff2e78fbd670d0b
Cassidy Schneider <71412336+cns0045@users.noreply.github.com>
2023-05-09 15:34:04 -0500

    Update 2_cleanTrim_QC.sh

--- scripts/2_cleanTrim_QC.sh
+++ scripts/2_cleanTrim_QC.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+##### Clean FASTQ files using Trimmomatic and evaluate with FastQC
+
 source /opt/asn/etc/asn-bash-profiles-special/modules.sh
 module load trimmomatic/0.39
 module load fastqc/0.11.9
@@ -37,14 +39,14 @@ done
 
 
 ############## FASTQC to assess quality of the sequence data
-## FastQC: run on each of the data files that have 'All' to check the quality of the data
+## FastQC: run on each of the data files to check the quality of the data
 ## The output from this analysis is a folder of results and a zipped file of results
+## Uses xargs to run 4 processors at a time
 
 cd $CLEANDIR
 
-echo */*.fastq.gz | xargs --max-procs=4 --max-args=1 --verbose fastqc --outdir=$POSTDIR
+echo */*_paired.fastq.gz | xargs --max-procs=4 --max-args=1 --verbose fastqc --outdir=$POSTDIR
 
-#######  Tarball the directory containing the FASTQC results so we can easily bring it back to our computer to evaluate.
-## when finished use scp or rsync to bring the tarballed .gz results file to your computer and open the .html file to evaluate the quality of your raw data.
+#######  Tarball the directory containing the FASTQC results
 cd $DIR
 tar cvzf PostCleanQuality.tar.gz $POSTDIR/*
